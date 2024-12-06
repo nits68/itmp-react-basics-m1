2b949127798e1f140a1ce157cb0b3aa6057f6661
nits68 <nits.laszlo@jedlik.eu>
2024-12-06 20:41:05 +0100

    finish m2

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,52 @@
+import OraCard from "./components/OraCard";
+import Oraszam from "./components/Oraszam";
+
+const orak = [
+  {
+    oraSzam: 1,
+    cim: "Bevezetés a webfejlesztésbe",
+    leiras: "Weboldalak működése és HTML áttekintés.",
+  },
+  {
+    oraSzam: 2,
+    cim: "Fejlesztői környezetek",
+    leiras:
+      "Az online és a professzionális fejlesztői eszközök áttekintése. A Visual Studio Code telepítése és alapvető használatának bemutatása.",
+  },
+  {
+    oraSzam: 3,
+    cim: "Alapvető HTML tagek",
+    leiras: "Legfontosabb tagek használata: h1-h6, p, img, a.",
+  },
+  {
+    oraSzam: 4,
+    cim: "HTML attribútumok és szövegformázás",
+    leiras: "Attribútumok: id, class, és szövegformázás.",
+  },
+  {
+    oraSzam: 5,
+    cim: "HTML listák és táblázatok",
+    leiras: "Listaelemek (ul, ol) és táblázatok (table).",
+  },
+];
+
 const App = () => {
   return (
     <main>
       <h1>Bevezetés a programozásba</h1>
-      <hr />
 
-      <section className="row">
-        <div className="text-lg">Teljes óraszám:</div>
-        <div className="row" style={{ gap: "0.5rem" }}>
-          <button className="icon-button">+</button>
-          <span className="font-semibold">32</span>
-          <button className="icon-button">-</button>
-        </div>
-      </section>
-
-      <section className="row">
-        <div className="text-lg">Hiányzó órák:</div>
-        <span className="font-semibold">29</span>
-      </section>
-
-      <hr />
-
-      <section style={{ padding: "0 2rem" }}>
-        <button className="temakor-button">Témakörök elrejtése</button>
-        <ul className="temakor-ul">
-          <li>Bevezetés a webfejlesztésbe: HTML és CSS alapok</li>
-          <li>
-            Fejlesztői környezetek és eszközök: Visual Studio Code, Git
-            használata
-          </li>
-          <li>
-            HTML tagek és attribútumok: weboldal-struktúra és tartalom
-            kialakítása
-          </li>
-          <li>
-            CSS alapjai: formázás, színek, box modell és reszponzív design
-          </li>
-          <li>JavaScript alapok: változók, események, és DOM manipuláció</li>
-          <li>Projektmunka: reszponzív weboldal tervezése és fejlesztése</li>
-          <li>Hibakeresési technikák: fejlesztői eszközök és validáció</li>
-          <li>Projektek bemutatása és értékelése</li>
-        </ul>
-      </section>
-
-      <hr />
-
-      <form action="#">
-        <div className="col">
-          <input type="text" placeholder="Cím" />
-          <textarea placeholder="Leírás" rows="5"></textarea>
-        </div>
-        <aside className="col">
-          <button className="btn">Hozzáadás</button>
-          <button className="btn outline">Mégsem</button>
-        </aside>
-      </form>
+      <Oraszam orakATanmenetben={orak.length} />
 
       <section className="ora-grid">
-        <article className="ora">
-          <header>
-            <h3>1. óra</h3>
-            <button className="icon-button">📝</button>
-            <button className="icon-button">🗑️</button>
-          </header>
-          <h4>Bevezetés a webfejlesztésbe</h4>
-          <p>Weboldalak működése és HTML áttekintés.</p>
-        </article>
+        {orak.map((ora) => (
+          <OraCard
+            key={ora.oraSzam}
+            oraSzam={`${ora.oraSzam}. óra`}
+            cim={ora.cim}
+          >
+            <p style={{fontStyle: "italic"}}>{ora.leiras}</p>
+          </OraCard>
+        ))}
       </section>
     </main>
   );
