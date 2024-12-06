fdb27ed830d3600c815da2690f005a6357503e8e
nits68 <nits.laszlo@jedlik.eu>
2024-12-06 21:49:36 +0100

    finish m3

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,52 @@
+import { useState } from "react";
 import OraCard from "./components/OraCard";
 import Oraszam from "./components/Oraszam";
-
-const orak = [
-  {
-    oraSzam: 1,
-    cim: "Bevezetés a webfejlesztésbe",
-    leiras: "Weboldalak működése és HTML áttekintés.",
-  },
-  {
-    oraSzam: 2,
-    cim: "Fejlesztői környezetek",
-    leiras:
-      "Az online és a professzionális fejlesztői eszközök áttekintése. A Visual Studio Code telepítése és alapvető használatának bemutatása.",
-  },
-  {
-    oraSzam: 3,
-    cim: "Alapvető HTML tagek",
-    leiras: "Legfontosabb tagek használata: h1-h6, p, img, a.",
-  },
-  {
-    oraSzam: 4,
-    cim: "HTML attribútumok és szövegformázás",
-    leiras: "Attribútumok: id, class, és szövegformázás.",
-  },
-  {
-    oraSzam: 5,
-    cim: "HTML listák és táblázatok",
-    leiras: "Listaelemek (ul, ol) és táblázatok (table).",
-  },
-];
+import Temakorok from "./components/Temakorok";
+import OraForm from "./components/OraForm";
 
 const App = () => {
+  const [orak, setOrak] = useState([
+    {
+      cim: "Bevezetés a webfejlesztésbe",
+      leiras: "Weboldalak működése és HTML áttekintés.",
+    },
+    {
+      cim: "Fejlesztői környezetek",
+      leiras:
+        "Az online és a professzionális fejlesztői eszközök áttekintése. A Visual Studio Code telepítése és alapvető használatának bemutatása.",
+    },
+    {
+      cim: "Alapvető HTML tagek",
+      leiras: "Legfontosabb tagek használata: h1-h6, p, img, a.",
+    },
+    {
+      cim: "HTML attribútumok és szövegformázás",
+      leiras: "Attribútumok: id, class, és szövegformázás.",
+    },
+    {
+      cim: "HTML listák és táblázatok",
+      leiras: "Listaelemek (ul, ol) és táblázatok (table).",
+    },
+  ]);
+
   return (
     <main>
       <h1>Bevezetés a programozásba</h1>
 
       <Oraszam orakATanmenetben={orak.length} />
+      <Temakorok />
+      <hr />
+      <OraForm onOraHozzaad={(newOra) => setOrak((p) => [...p, newOra])} />
 
       <section className="ora-grid">
-        {orak.map((ora) => (
+        {orak.map((ora, index) => (
           <OraCard
-            key={ora.oraSzam}
-            oraSzam={`${ora.oraSzam}. óra`}
+            key={index}
+            oraSzam={`${index + 1}. óra`}
             cim={ora.cim}
+            onKartyaTorles={() =>
+              setOrak((prev) => prev.filter((ora, i) => i !== index))
+            }
           >
             <p style={{fontStyle: "italic"}}>{ora.leiras}</p>
           </OraCard>
